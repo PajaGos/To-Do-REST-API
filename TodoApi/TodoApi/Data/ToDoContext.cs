03e850f82982b1cd9c855961a7b57604fe987f3f
Pavel Gos <pajagos@gmail.com>
2025-09-19 13:06:40 +0200

    feat: Add database

---
+++ TodoApi/TodoApi/Data/ToDoContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace TodoApi.Data
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> options) : base(options) { }
+
+        public DbSet<TaskItem> Tasks { get; set; }
+    }
+}
