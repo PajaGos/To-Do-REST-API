96a92ca2bd41e3882462e7ab6e05966020add86a
Pavel Gos <pajagos@gmail.com>
2025-09-26 15:57:28 +0200

    feat: Add users model and its controller with endpoints

---
+++ TodoApi/Controllers/UsersController.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Data;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [ApiController]
+    [Route("users")]
+    public class UsersController : ControllerBase
+    {
+        private readonly ToDoContext _context;
+        
+        public UsersController(ToDoContext context)
+        {
+            _context = context;
+        }
+        
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetAll() => await _context.Users.ToListAsync();
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetById(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            return user == null ? NotFound() : user;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> Create(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetById), new { id = user.Id }, user);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+            
+            User? existingUser = await _context.Users.FirstOrDefaultAsync(t => t.Id == id);
+
+            if (existingUser == null)
+            {
+                return NotFound();
+            }
+            
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
